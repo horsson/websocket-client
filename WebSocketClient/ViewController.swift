5cf30d9735d3db9d4081899f15614a84c62e6cb1
Hao Hu <hao.hu@sap.com>
2018-06-11 14:08:53 +0200

    intial import.

---
+++ WebSocketClient/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebSocketClient
+//
+//  Created by Hu, Hao on 11.06.18.
+//  Copyright © 2018 Hao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
